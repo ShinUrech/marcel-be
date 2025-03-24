f98ab9624a276dab7cb1d13a3e9afe627b89d0cd
Saifeddine Rhouma <saifeddine.rhouma91@gmail.com>
2025-03-24 10:15:09 +0100

    finished

--- src/modules/scraper/scraper.module.ts
+++ src/modules/scraper/scraper.module.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { ScraperController } from './controllers/scraper.controller';
 import { ScraperService } from './services/scraper.service';
